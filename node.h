136baa07a530696417606ff6de48f7f42265c311
marwan-9 <marwan.ahmed011@eng-st.cu.edu.eg>
2022-04-23 19:03:29 +0200

    PriortyQueue.c & Node.h

---
+++ node.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#ifndef MYNODE_H_
+#define MYNODE_H_
+typedef struct node {
+   int key;          //ID
+   int arrival_time;
+   int priority;
+   int runtime;
+   int data;
+   struct node *next;
+} node;
+#endif
---
+++ queue.c
@@ -0,0 +1,79 @@
+//C code to implement Priority Queue
+// using Linked List
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+ 
+// Node
+struct QNode {
+	int data;
+    struct node process; 
+  // int priority;
+    struct QNode* next;
+};
+
+
+// Function to Create A New Node
+struct QNode* newNode(struct node* input)
+{
+    struct QNode* temp = (struct QNode*)malloc(sizeof(struct QNode));
+    temp->process = *input;
+    temp->process.priority = input->priority;
+    temp->next = NULL;
+    return temp;
+}
+ 
+// Return the value at head
+struct node peek(struct QNode** head)
+{
+    return (*head)->process;
+}
+ 
+// Removes the element with the
+// highest priority form the list
+void DeQueue(struct QNode** head, struct node *Dequeued)
+{
+    struct QNode* temp = *head;
+	Dequeued = &((*head)->process);
+    (*head) = (*head)->next;
+
+    free(temp);
+}
+ 
+// Function to push according to priority
+void EnQueue(struct QNode** head,struct node input)
+{
+    struct QNode* start = (*head);
+    // Create new Node
+    struct QNode* temp = newNode(&input);
+    // Special Case: The head of list has lesser
+    // priority than new node. So insert new
+    // node before head node and change head node.
+	int p = input.priority;
+    if ((*head)->process.priority < p) {
+        // Insert New Node before head
+        temp->next = *head;
+        (*head) = temp;
+    }   
+
+    else {
+        // Traverse the list and find a
+        // position to insert new node
+        if (start !=NULL){
+        while (start->next!=NULL && (start->next)->process.priority < p) {
+            start = start->next;
+        }
+    }
+        // Either at the ends of the list
+        // or at required position
+        temp->next = start->next;
+        start->next = temp;
+
+    }
+}
+ 
+// Function to check is list is empty
+int isEmpty(struct QNode** head)
+{
+    return (*head) == NULL;
+}
